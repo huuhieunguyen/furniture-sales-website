4c2d7ca8bf2f29419fad58f750f7f91d1c046b69
UIT19520097 <67829285+UIT19520097@users.noreply.github.com>
2022-10-18 22:29:38 +0700

    section search

--- swiper/swiper.js
+++ swiper/swiper.js
@@ -1,17 +1,42 @@
-var swiper = new Swiper(".mySwiper", {
+var swiper1 = new Swiper(".mySwiper1", {
 	slidesPerView: 4,
-	spaceBetween: 30,
+	spaceBetween: 10,
 	slidesPerGroup: 1,
 	loop: true,
 	loopFillGroupWithBlank: true,
 	pagination: {
-		el: ".swiper-pagination",
+		el: ".my-custom-pagination-div",
 		clickable: true,
+		renderBullet: (index, className) => {
+			return '<span class="' + className + '">' + "</span>";
+		},
 	},
 	navigation: {
 		nextEl: ".swiper-button-next",
 		prevEl: ".swiper-button-prev",
 	},
 });
-swiper.loopDestroy();
-swiper.loopCreate();
+var swiper2 = new Swiper(".mySwiper2", {
+	slidesPerView: 3,
+	spaceBetween: 30,
+	centeredSlides: true,
+	grabCursor: true,
+	loop: true,
+	loopFillGroupWithBlank: true,
+	pagination: {
+		el: ".my-custom-pagination-div",
+		clickable: true,
+		renderBullet: (index, className) => {
+			return '<span class="' + className + '">' + "</span>";
+		},
+	},
+	autoplay: {
+		delay: 2000,
+		disableOnInteraction: false,
+	},
+});
+
+swiper1.loopDestroy();
+swiper1.loopCreate();
+swiper2.loopDestroy();
+swiper2.loopCreate();
