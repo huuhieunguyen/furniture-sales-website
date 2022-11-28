1fdad3385b79d280d9f0162d81abfd9b0b84aa9f
Hieu Nguyen <huuhieu.nguyen0408@gmail.com>
2022-11-28 10:15:14 +0700

    Add Back-end Project with ASP.NET Core MVC

---
+++ Funiture_Project/Areas/Admin/Controllers/ThongKeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Funiture_Project.Areas.Admin.Controllers
+{
+    public class ThongKeController : Controller
+    {
+        [Area("Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Funiture_Project/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Funiture_Project.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
