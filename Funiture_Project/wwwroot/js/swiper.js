fe7caf19973f6d0f29e3ec955e734b7d448f7206
UIT19520097 <67829285+UIT19520097@users.noreply.github.com>
2022-12-08 04:15:08 +0700

    responsive AboutUs CartInfoView LandingPage

--- Funiture_Project/wwwroot/js/swiper.js
+++ Funiture_Project/wwwroot/js/swiper.js
@@ -1,60 +1,60 @@
 var swiper1 = new Swiper(".mySwiper1", {
-	slidesPerView: 1,
-	spaceBetween: 10,
-	slidesPerGroup: 1,
-	loop: true,
-	loopFillGroupWithBlank: true,
-	pagination: {
-		el: ".my-custom-pagination-div",
-		clickable: true,
-		renderBullet: (index, className) => {
-			return '<span class="' + className + '">' + "</span>";
-		},
-	},
-	navigation: {
-		nextEl: ".swiper-button-next",
-		prevEl: ".swiper-button-prev",
-	},
-	breakpoints: {
-		768: {
-			slidesPerView: 2,
-			spaceBetween: 10,
-		},
-		1024: {
-			slidesPerView: 4,
-			spaceBetween: 30,
-		},
-	},
+    // slidesPerView: 1,
+    // spaceBetween: 10,
+    // slidesPerGroup: 1,
+    loop: true,
+    // loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".my-custom-pagination-div",
+        clickable: true,
+        renderBullet: (index, className) => {
+            return '<span class="' + className + '">' + "</span>";
+        },
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 30,
+        },
+    },
 });
 var swiper2 = new Swiper(".mySwiper2", {
-	slidesPerView: 1,
+    slidesPerView: 1,
 
-	spaceBetween: 30,
-	centeredSlides: true,
-	grabCursor: true,
-	loop: true,
-	loopFillGroupWithBlank: true,
-	pagination: {
-		el: ".my-custom-pagination-div",
-		clickable: true,
-		renderBullet: (index, className) => {
-			return '<span class="' + className + '">' + "</span>";
-		},
-	},
-	autoplay: {
-		delay: 1500,
-		disableOnInteraction: false,
-	},
-	breakpoints: {
-		768: {
-			slidesPerView: 1,
-			spaceBetween: 30,
-		},
-		1024: {
-			slidesPerView: 3,
-			spaceBetween: 30,
-		},
-	},
+    spaceBetween: 30,
+    centeredSlides: true,
+    grabCursor: true,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".my-custom-pagination-div",
+        clickable: true,
+        renderBullet: (index, className) => {
+            return '<span class="' + className + '">' + "</span>";
+        },
+    },
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        768: {
+            slidesPerView: 1,
+            spaceBetween: 30,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+    },
 });
 
 swiper1.loopDestroy();
