10ee920a0fd1566e5572a021160163e73189d48e
UIT19520097 <67829285+hmhung-2906@users.noreply.github.com>
2022-12-11 01:45:14 +0700

    Update CheckAll CartInfo

--- Funiture_Project/wwwroot/js/cart.js
+++ Funiture_Project/wwwroot/js/cart.js
@@ -4,41 +4,81 @@ const numList = document.querySelectorAll(".num");
 const rateList = document.querySelectorAll(".rate");
 const amountList = document.querySelectorAll(".amount");
 const sumCartEle = document.querySelector(".sum-cart");
-const wannaBuyAllBtn = document.getElementById("wanna-buy-all");
-
-wannaBuyAllBtn.onclick = function () {
-	checkboxes = document.getElementsByName("wannaBuy");
-	for (var i = 0, n = checkboxes.length; i < n; i++) {
-		checkboxes[i].checked = wannaBuyAllBtn.checked;
-	}
-};
-
+const wannaBuyAllBtn = document.getElementsByName("wanna-buy-all");
+const wannaBuyBtn = document.getElementsByName("wannaBuy");
+// Xử lý khi nhấn nút cộng số lượng sản phẩm
+for (let i = 0; i < plusBtnList.length; i++) {
+	plusBtnList[i].addEventListener("click", () => {
+		numList[i].value++;
+		amountList[i].innerHTML = handleSumPrice(rateList[i], numList[i]);
+		sumCart();
+	});
+}
+// Xử lý khi nhấn nút trừ số lượng sản phẩm
+for (let i = 0; i < minusBtnList.length; i++) {
+	minusBtnList[i].addEventListener("click", () => {
+		if (numList[i].value > 1) {
+			numList[i].value--;
+			amountList[i].innerHTML = handleSumPrice(rateList[i], numList[i]);
+			sumCart();
+		}
+	});
+}
+// Xử lý tính tổng tiền của giỏ hàng
 const sumCart = () => {
 	let sum = 0;
 	for (let i = 0; i < amountList.length; i++) {
-		sum = sum + parseInt(amountList[i].textContent);
+		if (wannaBuyBtn[i].checked) {
+			sum = sum + parseInt(amountList[i].textContent);
+		}
 	}
 	sumCartEle.innerHTML = String(sum);
 };
 sumCart();
-const handleClick = (rate, num) => {
-	console.log(String(parseInt(rate.textContent) * num.value));
+// Xử lý tính thành tiền của từng sản phẩm
+const handleSumPrice = (rate, num) => {
 	return String(parseInt(rate.textContent) * num.value);
 };
-for (let i = 0; i < plusBtnList.length; i++) {
-	plusBtnList[i].addEventListener("click", () => {
-		numList[i].value++;
-		console.log(handleClick(rateList[i], numList[i]));
-		amountList[i].innerHTML = handleClick(rateList[i], numList[i]);
-		sumCart();
-	});
-}
-for (let i = 0; i < minusBtnList.length; i++) {
-	minusBtnList[i].addEventListener("click", () => {
-		if (numList[i].value > 0) {
-			numList[i].value--;
-			amountList[i].innerHTML = handleClick(rateList[i], numList[i]);
-			sumCart();
+// Xử lý khi nhấn nút All
+const handleClickSelectAll = function (state, allChecked) {
+	// Trường hợp muốn hủy CheckAll
+	if (allChecked == false) {
+		for (let i = 0; i < wannaBuyAllBtn.length; i++) {
+			wannaBuyAllBtn[i].checked = false;
 		}
-	});
+		// Trường hợp muốn CheckAll
+	} else {
+		for (let i = 0; i < wannaBuyAllBtn.length; i++) {
+			wannaBuyAllBtn[i].checked = state;
+		}
+		for (let i = 0; i < wannaBuyBtn.length; i++) {
+			wannaBuyBtn[i].checked = state;
+		}
+	}
+	sumCart();
+};
+// Kiểm tra tất cả các checkbox có được check chưa
+const varifyCheckedAll = function () {
+	for (let i = 0; i < wannaBuyBtn.length; i++) {
+		if (wannaBuyBtn[i].checked == false) {
+			// Nếu có 1 check box là false thì sẽ hủy checkboxAll
+			return handleClickSelectAll(null, false);
+		}
+	}
+	// Nếu tất cả checkbox là true thì kích hoạt CheckAll
+	return handleClickSelectAll(true, true);
+};
+
+// Xử lý khi chọn checkbox sản phẩm để mua
+for (let i = 0; i < wannaBuyBtn.length; i++) {
+	wannaBuyBtn[i].onchange = function () {
+		varifyCheckedAll();
+		sumCart();
+	};
+}
+// Xử lý khi chọn checkbox all sản phẩm để mua
+for (let i = 0; i < wannaBuyAllBtn.length; i++) {
+	wannaBuyAllBtn[i].onchange = function () {
+		handleClickSelectAll(wannaBuyAllBtn[i].checked, true);
+	};
 }
